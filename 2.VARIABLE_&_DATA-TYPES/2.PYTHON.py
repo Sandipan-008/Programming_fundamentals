05e035354ed096ebd849ac011f1550c580e53dfa
Sandipan Majumder <sandmaj2001@gmail.com>
2024-07-19 10:30:13 +0530

    Update 2.PYTHON.py

--- 2.VARIABLE_&_DATA-TYPES/2.PYTHON.py
+++ 2.VARIABLE_&_DATA-TYPES/2.PYTHON.py
@@ -1,1 +1,65 @@
-                                                                            JAVA
+                                                                      Variable
+•	Declaration of variable:
+     Age = 25
+    Name = “jay”
+    Height = 5.7
+
+•	Printing the variable:
+print (Name)        #jay
+print (Age)            #25
+print (Height)	    #5.7
+
+
+
+•	Rules for Python Variable Names
+1.	Use only alphabets, numbers, and underscores (A-Z, a-z, 0-9, and _). Example: user_age, total_count2
+2.	Start with a letter or underscore, not a number. 
+Valid: _count, name1 
+Invalid: 1name, 234age
+3.	No spaces allowed.
+ Valid: first_name
+ Invalid: first name
+4.	Case-sensitive: age, Age, and AGE are three different variables.
+5.	Avoid using Python keywords (like if, for, while) as variable names.
+Valid: name = “rick”, adf = “2001”
+Invalid: for = 25, while = “goal”
+
+
+
+                                                                      Data type
+•	Integer (Int):
+	    age = 25 	#output: 25
+    	count = -10	#output: -10
+•	floating point (float):
+      pi = 3.14159     	#output:3.14159
+      temperature = -2.5   #output: -2.5
+      result = 0.1 + 0.2 print(result) # Output: 0.30000000000000004
+•	Boolean type:
+      is_student = True
+      has_license = False
+    if is_student:
+        print ("You are a student")
+    else:
+        print ("You are not a student")
+
+•	String:
+      name = "Alice” 	                           #output: Alice
+      message = 'Hello, World!'		#output: Hello, World!
+      Print (name, message)	             #output: Alice Hello, World!
+      full_name = "John " + "Doe"          #output: John Doe
+      print (full_name + “ is a man”)       #output: John Doe is a man
+•	List:
+       numbers = [1, 2, 3, 4, 5] 		#output: [1,2,3,4,5]
+       mixed = [1, "two", 3.0, [4, 5]]	#output: [1,”two”,3.0,[4,5]]
+•	Tuple:
+        coordinates = (10, 20)		#output: (10,20)
+        rgb = (255, 23, 3.40,(4,6,9))	#output: (255,23,3.40,(4,6,9))
+•	Dictionary:
+        person = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+        print(person['name'])     #Alice
+        print(person['age'])	    #30	
+        print(person['city'])	    #New York
+•	Set:
+       numbers = {1, 2, 3, 4, 5} 		#output:{1,2,3,4,5}
+       
+                                                                        
