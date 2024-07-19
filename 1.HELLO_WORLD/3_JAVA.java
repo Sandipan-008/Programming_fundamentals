ea30f716b4479e4ac8d2f1a22233697c107627df
Sandipan Majumder <sandmaj2001@gmail.com>
2024-07-19 10:28:05 +0530

    Update 3_JAVA.java

--- 1.HELLO_WORLD/3_JAVA.java
+++ 1.HELLO_WORLD/3_JAVA.java
@@ -1,1 +1,13 @@
+public class HELLO_WORLD{
+    public static void main(String args[]){
+        System.out.print("hello world2!");
+        System.out.print("hello world3!\n");
+        System.out.println("hello world4!");
+    }
+}
 
+Output:
+hello world2!hello world3 !
+hello world4 !
+  
+/*=== Code Execution Successful ===*/
